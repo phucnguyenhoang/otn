15bd9b844d935c0089e72a188db1177dbeb64c3b
phucnguyen <hoangphuc.ag@gmail.com>
2015-06-04 16:27:31 +0700

    make admin template

--- modules/admin/views/layout/footer.php
+++ modules/admin/views/layout/footer.php
@@ -1,7 +1,13 @@
+        </div>
+        <!-- /.container-fluid -->
+    </div>
+    <!-- /#page-wrapper -->
 </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="<?php echo base_url('resources/js/jquery-2.1.3.min.js'); ?>"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?php echo base_url('resources/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('modules/admin/resources/bower_components/metisMenu/dist/metisMenu.min.js'); ?>"></script>
+<script src="<?php echo base_url('modules/admin/resources/js/sb-admin-2.js'); ?>"></script>
 </body>
 </html>
