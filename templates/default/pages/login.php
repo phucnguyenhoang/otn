51ce652a4c6ea2a343776de64daf10d4d33c0b84
Le Vinh Phu <lvphu609@gmail.com>
2015-06-04 14:00:00 +0700

    modules login

---
+++ templates/default/pages/login.php
@@ -0,0 +1,10 @@
+<?php
+$regions['container'] = array(
+    'id' => 'pan_container',
+    'class' => 'pan-container container'
+);
+
+$regions['content'] = array(
+    'id' => 'pan_content',
+    'class' => 'pan-content'
+);
