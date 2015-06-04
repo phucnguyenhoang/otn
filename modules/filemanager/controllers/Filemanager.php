797486c058b8d9f1e8f4744ad6be7366a4839282
Le Vinh Phu <lvphu609@gmail.com>
2015-06-04 17:17:21 +0700

    file manager

---
+++ modules/filemanager/controllers/Filemanager.php
@@ -0,0 +1,23 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Filemanager extends MX_Controller {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+	public function index($params = array())
+	{
+		return $this->load->view('editor',$params);
+	}
+}
+
+/*	------usage--------
+	echo modules::run('filemanager',array(
+		'class' => 'box-content-article',
+		'id' => 'box-content-article',
+		'name' => 'box-content-article',
+		'content' => 'content input'
+	));
+*/
