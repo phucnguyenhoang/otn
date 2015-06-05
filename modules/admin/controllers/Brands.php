0d67472de6f0e7b16779675609ba74fb4a566660
Le Vinh Phu <lvphu609@gmail.com>
2015-06-05 17:47:27 +0700

    merge master

--- modules/admin/controllers/Brands.php
+++ modules/admin/controllers/Brands.php
@@ -4,18 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Brands extends MX_Controller {
     public function __construct() {
         parent::__construct();
+        $this->lang->load('common', 'admin');
         $this->lang->load('brand', 'admin');
     }
     public function index() {
         $header = array(
-            'css' => array(
-                'bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.css',
-                'bower_components/datatables-responsive/css/dataTables.responsive.css'
-            ),
-            'js' => array(
-                'bower_components/datatables/media/js/jquery.dataTables.min.js',
-                'bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js'
-            ),
             'lang_title' => $this->lang->line('title')
         );
         $content = array(
@@ -23,6 +16,21 @@ class Brands extends MX_Controller {
         );
         $this->load->view('layout/header', $header);
         $this->load->view('brands/index', $content);
+        $this->load->view('layout/footer');
+    }
+
+    public function create() {
+        $header = array(
+            'lang_title' => $this->lang->line('create_title')
+        );
+        $content = array(
+            'lang_title' => $this->lang->line('title'),
+            'lang_create' => $this->lang->line('create'),
+            'lang_btn_edit' => $this->lang->line('btn_edit'),
+            'lang_file_manager_title' => $this->lang->line('file_manager_title')
+        );
+        $this->load->view('layout/header', $header);
+        $this->load->view('brands/create', $content);
         $this->load->view('layout/footer');
     }
 }
--- modules/admin/resources/js/main.js
+++ modules/admin/resources/js/main.js
@@ -3,14 +3,49 @@ $(function() {
 });
 var Admin = {
     init: function() {
+        this._initModal();
         this._initDataTable();
+        this._initImageSelector();
     },
     eventHandle: function() {
-
+        this.onClickBtnImageSelector();
     },
     _initDataTable: function() {
         $('.data-tables').DataTable({
             responsive: true
+        });
+    },
+    _initImageSelector: function() {
+        var n = 0,
+            pattern = $('#img_selector_view_pattern');
+        $('.image-selector').each(function() {
+            n += 1;
+            var imgInput = $(this),
+                id = 'img_selector_' + n,
+                url = $('#frm_file_manager').attr('src') + '&field_id=' + id,
+                view = pattern.clone();
+
+            imgInput.attr('id', id);
+            view.removeAttr('id');
+            view.addClass('image-selector-view');
+            view.find('button').data({
+                id: id,
+                url: url
+            });
+            imgInput.after(view);
+        });
+    },
+    _initModal: function() {
+        $('#dialog_image_selector').modal({
+            show: false,
+            backdrop: 'static'
+        });
+    },
+    onClickBtnImageSelector: function() {
+        $(document).on('click', '.btn-open-image-selector', function() {
+            var btn = $(this);
+            $('#frm_file_manager').attr('src', btn.data('url'));
+            $('#dialog_image_selector').modal('show');
         });
     },
     run: function() {
@@ -26,4 +61,12 @@ var Verify = {
     run: function(){
         this.eventHandle();
     }
+}};
+function responsive_filemanager_callback(field_id){
+    var currInputImage = $('#'+field_id),
+        url = currInputImage.val();
+
+    url = url.replace('/files/source', '/files/thumbs');
+    currInputImage.next().css('backgroundImage', 'url("' + url + '")');
+    console.log(url);
 }
