1cd27d678f0fa3cdb4b8ef60c85cdc802f2d75f0
phucnguyen <hoangphuc.ag@gmail.com>
2015-06-09 14:31:26 +0700

    get support language for language library

--- application/libraries/Language.php
+++ application/libraries/Language.php
@@ -1,14 +1,34 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+/*
+ * {
+ *      alias: <string>,
+ *      name: <string>,
+ *      image: <string>,
+ *      sort: <number>,
+ *      status: <number>,
+ *      default: <number>
+ * }
+ */
+
 Class Language {
     const LANG = 'vn';
     public $lang;
+    private $default;
     public function __construct()
     {
         $this->CI =& get_instance();
         $this->CI->config->load('language');
         $this->lang = self::LANG;
+        $this->default = array(
+            'alias' => 'vn',
+            'name' => 'Việt Nam',
+            'image' => 'vn.png',
+            'sort' => 1,
+            'status' => 1,
+            'default' => 1
+        );
         $this->langSupport = $this->CI->config->item('lang');
     }
 
@@ -28,5 +48,21 @@ Class Language {
             $this->lang = self::LANG;
             $this->CI->document->setLang(self::LANG);
         }
+    }
+
+    public function getLang() {
+        $tplPath = APPPATH.'cache/language';
+        if (is_file($tplPath)) {
+            $result = array();
+            $lang = json_decode(file_get_contents($tplPath), true);
+            foreach ($lang as $row) {
+                unset($row['_id']);
+                $result[$row['sort']] = $row;
+            }
+            ksort($result);
+            return $result;
+        }
+
+        return $this->default;
     }
 }
