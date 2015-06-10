901d1f1c48897d0ed2326de5bcfeb368909a8186
phucnguyen <hoangphuc.ag@gmail.com>
2015-06-10 12:11:24 +0700

    delete draft module

--- templates/default/pages/home.php
+++ templates/default/pages/home.php
@@ -5,23 +5,5 @@ $regions['container'] = array(
 );
 $regions['header'] = array(
     'id' => 'pan_header',
-    'class' => 'pan-header row',
-    'regions' => array(
-        'menu' => array(
-            'id' => 'pan_menu',
-            'class' => 'pan-menu col-md-9'
-        ),
-        'search' => array(
-            'id' => 'pan-search',
-            'class' => 'pan-search col-md-3'
-        )
-    )
-);
-$regions['content'] = array(
-    'id' => 'pan_content',
-    'class' => 'pan-content row'
-);
-$regions['footer'] = array(
-    'id' => 'pan_footer',
-    'class' => 'pan-footer row'
+    'class' => 'pan-header row'
 );
