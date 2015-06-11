0fdc2755bcdd7bc7aeb5c3cc7cb6aa3643dd8ad8
phucnguyen <hoangphuc.ag@gmail.com>
2015-06-11 18:23:45 +0700

    add Product model

---
+++ modules/admin/models/Product_model.php
@@ -0,0 +1,94 @@
+<?php
+/*
+*** product ***
+{
+    <language> : {
+    "<_id>" : {
+        "alias": <string>,
+			"name" : <string>,
+			"catalog" : <string>,
+			"description" : {
+            "short" : <string>,
+				"long": <string>
+			},
+			"keyword": <string>,
+			"tag": <string>,
+			"categories": {
+            "<_id>" : {
+                "alias": <string>,
+					"name": <string>
+				}
+			},
+			"related": {
+            "<_id>" : {
+                "alias": <string>,
+					"name": <string>,
+					"description": <string>
+				}
+			},
+		}
+	}
+}
+*** product_description ***
+{
+    "code": <string>,
+	"prices": [
+		{
+            "color": <string>,
+			"size": <string>,
+			"label": <string>,
+			"price": <number>,
+			"quantity": <number>
+		}
+	],
+	"categories": [<_id>],
+	"minimum_quantity": <number>,
+	"date_available": <datetime>,
+	"required_shipping": <number>,
+	"length": <number>,
+	"width": <number>,
+	"height": <number>,
+	"weight": <number>,
+	"brands": [<string>],
+	"point_buy": <number>,
+	"reward": [
+		{
+            "group": <_id>,
+			"point": <number>
+		}
+	],
+	"out_of_stock": {
+    "id": <_id>,
+		"content": <number>
+	},
+	"tax": [
+		{
+            "id": <_id>,
+			"name": <string>,
+			"value": <number>,
+			"type": <number>
+		}
+	],
+	"related" : {
+    "<_id>": [
+			{
+                "color": <string>,
+				"size": <string>,
+				"label": <string>,
+				"price": <number>,
+				"quantity": <number>
+			}
+		]
+	},
+	"images" : [<string>],
+	"comments" : [
+		{
+            "owner": {"id": <_id>, "username": <string>},
+			"date": <datetime>,
+			"ranking": <number>,
+			"content": <string>
+		}
+	],
+	"status" : <number>
+}
+*/
